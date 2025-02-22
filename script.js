0ca2d962419c39c01b12ab374a1c49df24a17324
Abhishek Sikarwar <mail.abhisheksikarwar@gmail.com>
2025-02-22 17:55:50 +0530

    improved sorting function

--- script.js
+++ script.js
@@ -44,58 +44,58 @@ function getCryptoCurrencyData() {
 //     handling fetch promise using async await
 
 async function getCryptoCurrencyData() {
-  try {
-    const response = await fetch(API_LINK);
+    try {
+        const response = await fetch(API_LINK);
 
-    // checking response of fetch request
-    if (!response.ok)
-      throw new Error(
-        "Error in retrieving data from API link. Rate limit error."
-      );
+        // checking response of fetch request
+        if (!response.ok)
+            throw new Error(
+                "Error in retrieving data from API link. Rate limit error."
+            );
 
-    const cryptoData = await response.json();
+        const cryptoData = await response.json();
 
-    //data received is put into global array cryptoCurrencyData
-    cryptoCurrencyData = filteredArray = cryptoData;
+        //data received is put into global array cryptoCurrencyData
+        cryptoCurrencyData = filteredArray = cryptoData;
 
-    // function to insert fetched data into the container and displaying it
-    insertDataInContainer(cryptoCurrencyData);
+        // function to insert fetched data into the container and displaying it
+        insertDataInContainer(cryptoCurrencyData);
 
-    // if data is received we remove error message from screen
-    no_data_received.classList.add("hidden");
-  } catch (err) {
-    // display any error occured during fetch
-    console.log(err.message);
+        // if data is received we remove error message from screen
+        no_data_received.classList.add("hidden");
+    } catch (err) {
+        // display any error occured during fetch
+        console.log(err.message);
 
-    // if data is not received we display error message on screen
-    no_data_received.classList.remove("hidden");
-  }
+        // if data is not received we display error message on screen
+        no_data_received.classList.remove("hidden");
+    }
 }
 
 // function to insert data in container
 function insertDataInContainer(cryptoData) {
-  cryptoContainer.innerHTML = "";
-  cryptoData.map((crypto) => {
-    const row = insertInRow(crypto);
-    cryptoContainer.insertAdjacentElement("beforeend", row);
-  });
+    cryptoContainer.innerHTML = "";
+    cryptoData.map((crypto) => {
+        const row = insertInRow(crypto);
+        cryptoContainer.insertAdjacentElement("beforeend", row);
+    });
 }
 
 // function to insert data in table row
 function insertInRow(crypto) {
-  const {
-    id,
-    image,
-    name,
-    symbol,
-    current_price,
-    total_volume,
-    price_change_percentage_24h: price_change,
-    market_cap,
-  } = crypto;
+    const {
+        id,
+        image,
+        name,
+        symbol,
+        current_price,
+        total_volume,
+        price_change_percentage_24h: price_change,
+        market_cap,
+    } = crypto;
 
-  const tr = document.createElement("tr");
-  const markup = `<td>
+    const tr = document.createElement("tr");
+    const markup = `<td>
                 <div class="col-1">
                   <img
                     class="logo_img"
@@ -116,71 +116,73 @@ function insertInRow(crypto) {
               </td>
               <td>
                 <span class='col-5 ${
-                  price_change < 0 ? "neg" : "pos"
+                    price_change < 0 ? "neg" : "pos"
                 }' >${price_change.toFixed(2)}%</span>
               </td>
               <td>
                 <span class="col-6">Mkt Cap : $${formatCurrency(
-                  market_cap
+                    market_cap
                 )}</span>
               </td>
               `;
-  tr.insertAdjacentHTML("beforeend", markup);
-  return tr;
+    tr.insertAdjacentHTML("beforeend", markup);
+    return tr;
 }
 
 // function to format currency depending upon the locale of the user
 function formatCurrency(currency) {
-  return currency.toLocaleString();
+    return currency.toLocaleString();
 }
 
 // sort the data based on type(increasing or decreasing) and value(market cap or percentage)
 const sortBy = function (type, value) {
-  switch (type) {
-    case "inc": {
-      filteredArray.sort((a, b) => a[value] - b[value]);
-      break;
-    }
-    case "dec": {
-      filteredArray.sort((a, b) => b[value] - a[value]);
-      break;
-    }
-    default:
-      break;
-  }
+    const modifier = type === "inc" ? 1 : -1;
+    filteredArray.sort((a, b) => (a[value] - b[value]) * modifier);
+    // switch (type) {
+    //     case "inc": {
+    //         filteredArray.sort((a, b) => a[value] - b[value]);
+    //         break;
+    //     }
+    //     case "dec": {
+    //         filteredArray.sort((a, b) => b[value] - a[value]);
+    //         break;
+    //     }
+    //     default:
+    //         break;
+    // }
 };
 
 // function to handle the sort by market cap functionality
 function handlesortByCap() {
-  isIncCap ? sortBy("inc", "market_cap") : sortBy("dec", "market_cap");
+    isIncCap ? sortBy("inc", "market_cap") : sortBy("dec", "market_cap");
 
-  // when user repeatedly click on sort button the data is sorted in ascending or descending order on adjacent clicks
-  isIncCap = !isIncCap;
-  insertDataInContainer(filteredArray);
+    // when user repeatedly click on sort button the data is sorted in ascending or descending order on adjacent clicks
+    isIncCap = !isIncCap;
+    insertDataInContainer(filteredArray);
 }
 
 // function to handle the sort by percentage functionality
 function handlesortByPercentage() {
-  isIncPercentage
-    ? sortBy("inc", "price_change_percentage_24h")
-    : sortBy("dec", "price_change_percentage_24h");
+    isIncPercentage
+        ? sortBy("inc", "price_change_percentage_24h")
+        : sortBy("dec", "price_change_percentage_24h");
 
-  // when user repeatedly click on sort button the data is sorted in ascending or descending order on adjacent clicks
-  isIncPercentage = !isIncPercentage;
-  insertDataInContainer(filteredArray);
+    // when user repeatedly click on sort button the data is sorted in ascending or descending order on adjacent clicks
+    isIncPercentage = !isIncPercentage;
+    insertDataInContainer(filteredArray);
 }
 
 // function to handle the search functionality
 function handleChangeInInput() {
-  const nameOrsymbol = this.value.toLowerCase();
-  filteredArray = cryptoCurrencyData.filter((crypto) => {
-    return (
-      crypto.name.toLowerCase().includes(nameOrsymbol) ||
-      crypto.symbol.toLowerCase().includes(nameOrsymbol)
-    );
-  });
+    const nameOrsymbol = this.value.toLowerCase();
+    filteredArray = cryptoCurrencyData.filter((crypto) => {
+        return (
+            crypto.name.toLowerCase().includes(nameOrsymbol) ||
+            crypto.symbol.toLowerCase().includes(nameOrsymbol)
+        );
+    });
 
-  insertDataInContainer(filteredArray);
+    insertDataInContainer(filteredArray);
 }
 
 // on load we call getCryptoCurrencyData function to make a fetch request and get the data from api.
